dab0296c76ab85640f2579178bd6f95a19428784
Marquis Kurt <software@marquiskurt.net>
2025-02-16 15:54:10 -0500

    #2: Add unit tests for structure names

---
+++ MCMapsTests/MinecraftStructureNameTests.swift
@@ -0,0 +1,25 @@
+//
+//  MinecraftStructureNameTests.swift
+//  MCMaps
+//
+//  Created by Marquis Kurt on 16-02-2025.
+//
+
+import CubiomesKit
+import Foundation
+import Testing
+
+@testable import Alidade
+
+struct MinecraftStructureNameTests {
+    @Test(arguments: [
+        ("Mineshaft", MinecraftStructure.mineshaft),
+        ("Trial Chamber", MinecraftStructure.trialChambers),
+        ("End City", MinecraftStructure.endCity)
+    ])
+    func nameReturnsValidMatch(name: String, expected: MinecraftStructure) async throws {
+        let potentialStruct = MinecraftStructure(string: name)
+        #expect(potentialStruct == expected)
+        #expect(potentialStruct?.name == name)
+    }
+}
