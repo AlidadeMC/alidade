c3fdbfd9cdaae843f2899f093341cf5633045d7a
Marquis Kurt <software@marquiskurt.net>
2025-02-24 18:09:43 -0500

    Add debug for snapshots (take two)
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,4 +3,4 @@
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 
-ls CubiomesKit/Tests/CubiomesKitTests/__Snapshots__/CubiomesKitTests
+ls ../CubiomesKit/Tests/CubiomesKitTests/__Snapshots__/CubiomesKitTests
