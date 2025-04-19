ed4701e6b19603533a551b68715d5c17bb53ecb1
Marquis Kurt <software@marquiskurt.net>
2025-04-19 17:14:35 -0400

    Update ci_post_clone.sh
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,5 +2,3 @@
 
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
-
-ls ../CubiomesKit/Tests/CubiomesKitTests/__Snapshots__/CubiomesKitTests
