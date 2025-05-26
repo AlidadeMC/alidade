30da431b14f51b2cf1b9ffe982cb92928f71b8a7
Marquis Kurt <software@marquiskurt.net>
2025-05-26 15:21:13 -0400

    Fix DesignLibrary dependency pathing?

--- AlidadeUI/Package.swift
+++ AlidadeUI/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/nalexn/ViewInspector", from: "0.10.0"),
-        .package(name: "DesignLibrary", path: "DesignLibrary"),
+        .package(name: "DesignLibrary", path: "../DesignLibrary"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
