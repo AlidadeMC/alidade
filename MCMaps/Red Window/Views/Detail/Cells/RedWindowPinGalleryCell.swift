dac122c7c0961133792fc17e739f18759eefec4c
Marquis Kurt <software@marquiskurt.net>
2025-07-25 13:38:06 -0400

    Allow previewing images in detail on Red Window
    Tapping on a photo in a pin under the Red Window redesign will trigger
    Quick Look to preview the file.

--- MCMaps/Red Window/Views/Detail/Cells/RedWindowPinGalleryCell.swift
+++ MCMaps/Red Window/Views/Detail/Cells/RedWindowPinGalleryCell.swift
@@ -7,11 +7,14 @@
 
 import MCMap
 import SwiftUI
+import QuickLook
 
 /// The cell used to display the images associated with this pin.
 ///
 /// This is a read-only cell, and the ``isEditing`` property has no effect on this view.
 struct RedWindowPinGalleryCell: RedWindowDetailCell {
+    @Environment(\.documentURL) private var documentURL
+
     /// The pin from whose images are being displayed.
     @Binding var pin: CartographyMapPin
 
@@ -21,6 +24,8 @@ struct RedWindowPinGalleryCell: RedWindowDetailCell {
 
     /// The file that contains the pin and images being displayed.
     @Binding var file: CartographyMapFile
+
+    @State private var imageToPreview: URL?
 
     var body: some View {
         Group {
@@ -33,23 +38,33 @@ struct RedWindowPinGalleryCell: RedWindowDetailCell {
                     HStack {
                         ForEach(Array(images), id: \.self) { image in
                             if let imageData = getImage(named: image) {
-                                Image(data: imageData)
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(minHeight: 150, maxHeight: 200)
-                                    .clipped()
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                                Button {
+                                    imageToPreview = getUrl(forImageNamed: image)
+                                } label: {
+                                    Image(data: imageData)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(minHeight: 150, maxHeight: 200)
+                                        .clipped()
+                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                }
                             }
                         }
+                        .buttonStyle(.plain)
                     }
                 }
             } else {
                 ContentUnavailableView("No Photos Uploaded", systemImage: "photo.stack")
             }
         }
+        .quickLookPreview($imageToPreview)
     }
 
     private func getImage(named name: String) -> Data? {
         return file.images[name]
+    }
+
+    private func getUrl(forImageNamed name: String) -> URL? {
+        documentURL?.appending(components: CartographyMapFile.Keys.images, name)
     }
 }
