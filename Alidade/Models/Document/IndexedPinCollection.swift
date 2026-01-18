05bf5aa97a80314fa96c6f0bc7dab0bf22d7b5a4
Marquis Kurt <software@marquiskurt.net>
2026-01-18 14:58:09 -0500

    Leverage Bedrock's IndexedCollection

--- Alidade/Models/Document/IndexedPinCollection.swift
+++ Alidade/Models/Document/IndexedPinCollection.swift
@@ -5,6 +5,7 @@
 //  Created by Marquis Kurt on 18-06-2025.
 //
 
+import Bedrock
 import Foundation
 import MCMap
 
@@ -26,74 +27,4 @@ import MCMap
 ///     }
 /// }
 /// ```
-struct IndexedPinCollection {
-    fileprivate typealias InternalContainer = [CartographyMapPin]
-    /// A structure representing an element in the collection.
-    struct Element: Identifiable, Equatable, Hashable {
-        /// A unique identifier for the element.
-        var id: Int { index }
-
-        /// The index where the pin can be found in the source list.
-        var index: Int
-
-        /// The contents of the pin.
-        var content: CartographyMapPin
-    }
-
-    private var elements: [CartographyMapPin]
-
-    /// Create a collection from an existing array of pins.
-    init(_ elements: [CartographyMapPin]) {
-        self.elements = elements
-    }
-}
-
-extension IndexedPinCollection: Codable {
-    func encode(to encoder: any Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encode(elements)
-    }
-
-    init(from decoder: any Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        self.elements = try container.decode(InternalContainer.self)
-    }
-}
-
-extension IndexedPinCollection: Equatable {}
-
-extension IndexedPinCollection: Hashable {
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(elements)
-    }
-}
-
-extension IndexedPinCollection: ExpressibleByArrayLiteral {
-    typealias ArrayLiteralElement = CartographyMapPin
-
-    init(arrayLiteral elements: CartographyMapPin...) {
-        self.init(elements)
-    }
-}
-
-extension IndexedPinCollection: Collection {
-    typealias Index = [CartographyMapPin].Index
-
-    var startIndex: Array<CartographyMapPin>.Index {
-        elements.startIndex
-    }
-
-    var endIndex: Array<CartographyMapPin>.Index {
-        elements.endIndex
-    }
-
-    subscript(position: Array<CartographyMapPin>.Index) -> Element {
-        return Element(index: Int(position), content: elements[position])
-    }
-
-    func index(after index: Array<CartographyMapPin>.Index) -> Array<CartographyMapPin>.Index {
-        elements.index(after: index)
-    }
-}
-
-extension IndexedPinCollection: RandomAccessCollection {}
+typealias IndexedPinCollection = IndexedCollection<CartographyMapPin>
